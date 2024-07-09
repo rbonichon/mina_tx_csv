72554a2ac107c8453c01e52db2507f839baaa8f7
Richard Bonichon <richard.bonichon@gmail.com>
2024-07-09 21:48:09 +0000

    Configuration

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> { }
+}:
+
+let
+  camlpkgs = pkgs.ocamlPackages;
+  ocamlDevelPackages = with pkgs;
+    [
+      dune_3
+      ocamlformat
+      camlpkgs.merlin
+      camlpkgs.findlib
+      camlpkgs.calendar
+      camlpkgs.xml-light
+      camlpkgs.cohttp
+      camlpkgs.cohttp-lwt
+      camlpkgs.cohttp-lwt-unix
+      camlpkgs.ezjsonm
+      camlpkgs.tezos-base58
+    ];
+in
+rec {
+  devshell = pkgs.mkShell {
+    buildInputs = with pkgs; [
+
+    ] ++ ocamlDevelPackages;
+  };
+}
+
